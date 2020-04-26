2ead4a67f53f6248261c4219028da5775344addc
jethro riosa <jethro.riosa@gmail.com>
2020-04-26 22:08:14 +0800

    added test

--- screens/__tests__/HomeScreen-test.tsx
+++ screens/__tests__/HomeScreen-test.tsx
@@ -1,13 +1,14 @@
 /**
  * @format
  */
+
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import HomeScreen from '../HomeScreen';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<HomeScreen />);
 });
