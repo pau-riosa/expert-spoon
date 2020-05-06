e08bc15f012edf19bd7c1bb8c305eb089fec5a87
jethro riosa <jethro.riosa@gmail.com>
2020-05-06 22:36:06 +0800

    add design appointment info

--- screens/Root/Appointments/AppointmentInfoScreen.tsx
+++ screens/Root/Appointments/AppointmentInfoScreen.tsx
@@ -28,12 +28,56 @@ class AppointmentInfoScreen extends React.Component<IProps, IState> {
       <View style={{ flex: 1 }}> 
         <StatusBar barStyle="dark-content" backgroundColor={colors.ocean1}  />
         <SafeAreaView style={{ flex: 1, backgroundColor: colors.ocean5 }}>
-          <View style={styles.verticalContainer}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-              <View style={styles.scrollViewVerticalStyle}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.container}>
+              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', padding: 5}}>
+                <Text style={styles.mainText}>Appointment</Text>
               </View>
-            </ScrollView>
-          </View>
+              <View style={{ flex: 1, flexDirection: 'row', padding: 5}}>
+                <View style={{ flex: 0.5, flexDirection: 'column' }}>
+                  <Text style={styles.subText}>Name: </Text>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column' }}>
+                  <Text style={styles.text}>Juan Dela Cruz</Text>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'row', padding: 5}}>
+                <View style={{ flex: 0.5, flexDirection: 'column' }}>
+                  <Text style={styles.subText}>Agenda: </Text>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column' }}>
+                  <Text style={styles.text}>Check up with Mrs. Cruz</Text>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'row', padding: 5}}>
+                <View style={{ flex: 0.5, flexDirection: 'column' }}>
+                  <Text style={styles.subText}>Date: </Text>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column' }}>
+                  <Text style={styles.text}>2020-01-01</Text>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', padding: 5}}>
+                <Text style={styles.subText}>Description: </Text>
+              </View>
+              <ScrollView showsVerticalScrollIndicator={true} style={{ height: 400, padding: 5 }}>
+                <Text style={styles.description}> 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida dictum fusce ut placerat orci. Consequat nisl vel pretium lectus quam. Massa tincidunt dui ut ornare lectus. Pharetra convallis posuere morbi leo. Fames ac turpis egestas integer. Tellus rutrum tellus pellentesque eu tincidunt tortor. Duis at consectetur lorem donec massa sapien faucibus et molestie. Sit amet dictum sit amet justo. Neque vitae tempus quam pellentesque nec nam aliquam.
+
+Sed sed risus pretium quam vulputate dignissim suspendisse. Tristique senectus et netus et malesuada fames ac turpis egestas. Aliquet risus feugiat in ante metus dictum at tempor commodo. Nulla facilisi cras fermentum odio. Neque volutpat ac tincidunt vitae semper quis lectus nulla at. Nec sagittis aliquam malesuada bibendum arcu vitae. Velit ut tortor pretium viverra. Mattis molestie a iaculis at erat pellentesque adipiscing. Facilisi morbi tempus iaculis urna id. Sagittis orci a scelerisque purus semper eget duis at. Ac orci phasellus egestas tellus. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Faucibus et molestie ac feugiat sed.
+
+Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. In vitae turpis massa sed elementum tempus egestas sed sed. Ac felis donec et odio pellentesque diam. Fames ac turpis egestas sed tempus urna et pharetra. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Gravida neque convallis a cras semper auctor neque vitae tempus. Diam in arcu cursus euismod. Eget nullam non nisi est sit. Gravida quis blandit turpis cursus. Nibh nisl condimentum id venenatis a condimentum vitae. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Donec adipiscing tristique risus nec. Feugiat scelerisque varius morbi enim. Et tortor consequat id porta nibh venenatis cras sed felis. Rutrum tellus pellentesque eu tincidunt tortor. Turpis egestas maecenas pharetra convallis posuere.
+
+Semper eget duis at tellus at urna. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla. Porttitor rhoncus dolor purus non enim. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Sit amet tellus cras adipiscing enim eu turpis egestas. Mauris a diam maecenas sed enim. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Integer eget aliquet nibh praesent tristique magna sit amet. Mauris cursus mattis molestie a iaculis at erat. Eget nullam non nisi est sit amet facilisis. Netus et malesuada fames ac turpis egestas integer.
+
+Nulla pharetra diam sit amet nisl. Ut lectus arcu bibendum at. Vitae suscipit tellus mauris a diam maecenas sed enim ut. Elementum eu facilisis sed odio. Facilisis sed odio morbi quis commodo odio. In iaculis nunc sed augue lacus viverra. Lacus sed turpis tincidunt id aliquet. Mauris vitae ultricies leo integer malesuada nunc vel. Tempor orci eu lobortis elementum nibh tellus molestie. Volutpat ac tincidunt vitae semper. Felis bibendum ut tristique et egestas quis ipsum. A pellentesque sit amet porttitor eget dolor morbi non arcu. Integer malesuada nunc vel risus commodo. Platea dictumst quisque sagittis purus sit. Nunc mattis enim ut tellus elementum sagittis. Varius vel pharetra vel turpis nunc eget. Non tellus orci ac auctor augue mauris augue neque. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada.
+                </Text>
+              </ScrollView>
+              <View style={{ flex: 1, flexDirection: 'row', paddingVertical: 20, paddingHorizontal: 5}}>
+                <Button title="UPDATE" type="outline" buttonStyle={styles.buttonStyle} titleStyle={styles.titleStyle} />
+              </View>
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </View>
     );
@@ -43,17 +87,19 @@ export default AppointmentInfoScreen;
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
-  verticalContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1, 
-    backgroundColor: "#e5e5e5",
+  titleStyle: {
+    color: colors.ocean1,
+    fontFamily: 'Gill Sans',
+    fontSize: 18
   },
-  horizontalContainer: {
-    height: '30%', 
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#e5e5e5",
+  buttonStyle: {
+    borderWidth: 0, 
+  },
+  container: {
+    flex: 1,
+    padding: 20, 
+    flexDirection: 'column',
+    backgroundColor: "#FFFFFF",
   },
   headerText: {
     fontSize: 30,
@@ -67,37 +113,38 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column'
   },
-  logoContainer: {
-    flex: 0.2,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.ocean5
-  },
   flex1: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-evenly',
     paddingHorizontal: 15,
-    backgroundColor: colors.ocean5
+    backgroundColor: colors.ocean5,
   },
   mainText: {
     fontFamily: 'Gill Sans',
-    fontSize: 50,
-    fontWeight: '300',
-    color: '#363A44'
+    fontSize: 25,
+    marginBottom: 10,
+    fontWeight: '400',
+    color: colors.black,
   },
   subText: {
-    marginHorizontal: 10, 
     fontFamily: 'Gill Sans',
-    fontSize: 18,
-    fontWeight: '500',
-    color: '#363A44'
+    fontSize: 16,
+    color: colors.black,
   },
   text: {
-    fontFamily: 'Gill Sans', 
-    fontSize: 17, 
-    marginVertical: 2 
+    fontFamily: 'Roboto-Regular', 
+    fontSize: 16, 
+    color: colors.black,
+    fontWeight: '300',
+    marginHorizontal: 10,
+  },
+  description: {
+    fontFamily: 'Roboto-Regular', 
+    fontSize: 16,
+    color: colors.black,
+    fontWeight: '300',
+    textAlign: 'justify',
   }
 });
 
