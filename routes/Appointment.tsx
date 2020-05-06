5d9b4585d09f3fdce7131f8d20c874b3717fc4b1
jethro riosa <jethro.riosa@gmail.com>
2020-05-06 22:49:41 +0800

    able to show icons

--- routes/Appointment.tsx
+++ routes/Appointment.tsx
@@ -3,36 +3,56 @@
  */
 import * as React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {Icon} from 'react-native-elements';
 // Screens
 import AppointmentMessageScreen from '../screens/Root/Appointments/AppointmentMessageScreen';
 import AppointmentInfoScreen from '../screens/Root/Appointments/AppointmentInfoScreen';
 import AppointmentAttachmentScreen from '../screens/Root/Appointments/AppointmentAttachmentScreen';
-
+// colors
+import {colors} from '../assets';
 type NavigatorParams = {
   APPOINTMENT_INFO: undefined;
   APPOINTMENT_MESSAGE: undefined;
   APPOINTMENT_ATTACHMENT: undefined;
 };
-
 const TopTab = createMaterialTopTabNavigator<NavigatorParams>();
-
 export default function Appointment() {
   return (
-    <TopTab.Navigator initialRouteName="APPOINTMENT_INFO">
+    <TopTab.Navigator 
+      initialRouteName="APPOINTMENT_INFO"
+      tabBarOptions={{
+        showIcon: true,
+        showLabel: false,
+        activeTintColor: colors.ocean1,
+        inactiveTintColor: colors.primaryGrey,
+      }}
+    >
       <TopTab.Screen
         name="APPOINTMENT_INFO"
         component={AppointmentInfoScreen}
-        options={{ title: 'INFO' }}
+        options={{ 
+          tabBarIcon: ({color}) => (
+            <Icon name="user-circle" size={24} type="font-awesome" color={color} />
+          ),
+        }}
       />
       <TopTab.Screen
         name="APPOINTMENT_MESSAGE"
         component={AppointmentMessageScreen}
-        options={{ title: 'MESSAGE' }}
+        options={{ 
+          tabBarIcon: ({color}) => (
+            <Icon name="chat-bubble" size={24} type="material-community-icon" color={color} />
+          ),
+        }}
       />
       <TopTab.Screen
         name="APPOINTMENT_ATTACHMENT"
         component={AppointmentAttachmentScreen}
-        options={{ title: 'ATTACHMENT' }}
+        options={{ 
+          tabBarIcon: ({color}) => (
+            <Icon name="folder-open" size={24} type="font-awesome" color={color} />
+          ),
+        }}
       />
     </TopTab.Navigator>
   );
